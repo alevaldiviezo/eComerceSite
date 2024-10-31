77c6ac6adf9cf1d421951df92bcf5ded3b52dcfe
alevaldiviezo <alevaldiviezo@gmail.com>
2024-10-31 17:37:18 +0000

    reverse items

--- e-comerce-site/src/components/Content.tsx
+++ e-comerce-site/src/components/Content.tsx
@@ -90,6 +90,41 @@ const Content: React.FC = () => {
             buyLink:'https://amzn.to/407s8Hf',
             description: "ENHANCED BIOAVAILABILITY – Multivita magnesium capsules contain 1480mg of Magnesium Citrate and 440mg of Elemental Magnesium, the most bioavailable form of magnesium, per serving of 2 easy to swallow capsules. Each pouch is supplied with 120 easy-to-swallow vegan capsules that will last for 2 months. "
         },
+        {
+            index: 11,
+            name:"Fast Boil Electric Kettle, 2L, 2000 Watts, Glass Kettle",
+            image:'https://m.media-amazon.com/images/I/71DUhBQ93UL._AC_SL1500_.jpg',
+            buyLink:'https://amzn.to/40Kskwz',
+            description: "Easy to Clean, Auto Shut-Off and Boil-Dry Protection, BPA Free. Our electric kettle is made with high-quality borosilicate glass, and 304 stainless steel to keep your water safe and tasting pure for years to come."
+        },
+        {
+            index: 12,
+            name:"Acer Aspire 5 A515-57 Laptop - Intel Core i5-12450H, 16GB, 512GB SSD, Integrated Graphics, 15.6-inch FHD IPS, Windows 11, Iron ",
+            image:'https://m.media-amazon.com/images/I/81JpKd-ml2L._AC_SL1500_.jpg',
+            buyLink:'https://amzn.to/40qLtmI',
+            description: "POWERFUL PERFORMANCE: The combined performance of a 12th Gen Intel Core i5 CPU and 16GB of RAM means the Aspire 5 will breeze through the most demanding of tasks "
+        },
+        {
+            index: 13,
+            name:"Hisense 40 inch Full HD QLED Smart TV 40A5NQTUK - Quantum Dot Colour, Natural Enhancer,HDR & HLG-Dolby Atoms, HDMI, Share to TV, and Youtube, Freeview Play, Netflix and Disney+ (2024 Model) ",
+            image:'https://m.media-amazon.com/images/I/71wppCYPwFL._AC_SL1500_.jpg',
+            buyLink:'https://amzn.to/4f3U5V6',
+            description: " Picture Quality: Quantum Dot Technology | Full HD (1920x1080) | Refresh Rate: 60 hertz | 178 wide viewing angle | AI Sports Mode Sound: 16 Watts Speakers | DTS Virtual: X | Dolby Atmos "
+        },
+        {
+            index: 14,
+            name:"Tefal Express Essential Steam Generator Iron, 120 g/min Steam Output, 340 g/min Steam Boost, 5.9-Pump Bars with Scale Rinsing System, SV6132G0 ",
+            image:'https://m.media-amazon.com/images/I/71sGfy9WyHL._AC_SL1500_.jpg',
+            buyLink:'https://amzn.to/3Ys0kee',
+            description: "  Express results: 5.9-bar pump, 120g/min continuous steam and 340g boost for fast and effortless results. Shorter sessions: Up to 30% faster ironing* along with a rapid 2-minute heat-up time. *versus a traditional steam iron. Non-stop ironing: A generous 1.7L removable water tank for uninterrupted sessions and easy refills. "
+        },
+        {
+            index: 15,
+            name:"14 inch Laptop Screen Extender, Dual Screen Laptop Monitor Extender,1080P FHD IPS Laptop Portable Monitor for Laptops 13-17.3 inch, Suit for Windows Mac Android PS5 etc (black) ",
+            image:'https://m.media-amazon.com/images/I/81YhBOp8auL._AC_SL1500_.jpg',
+            buyLink:'https://amzn.to/3AmQn9S',
+            description: "  Dual monitor for laptop screen extender supports you to open multiple windows at the same time, work, video conference, watch movies, or play games on different screens by not having to switch back and forth on a single laptop screen continuously, increase your efficient by up to 200%. Perfect for people who work on the go，remote workers, watch a movie，IT professionals, Players, Stock traders, Programmers, game players, etc! "
+        },
     ];
     
     return (
@@ -105,7 +140,7 @@ const Content: React.FC = () => {
                 />
             </div>
             <div className="container">
-                {links
+                {links.reverse()
                     .filter(item => item.name.toLowerCase().includes(searchTerm.toLowerCase())) // Filter cards
                     .map((item, index) => (
                         <div key={index} className="card" onClick={()=>open(item.buyLink)}>
